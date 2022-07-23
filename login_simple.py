0da5a4beffecfdeae195a6e7c1e91a057b927e69
Frankie Guapo <frankie.guapo@gmail.com>
2022-07-23 10:06:20 -0500

    first commit

---
+++ login_simple.py
@@ -0,0 +1,38 @@
+"""Test login on Scotty Cameron site"""
+
+import os
+import time
+
+import undetected_chromedriver as uc
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+
+    
+def login_user(username, password):
+    LOGIN_URL = "https://www.scottycameron.com/store/user/login/"
+
+    driver = uc.Chrome()
+
+    driver.get(LOGIN_URL)
+
+    wait = WebDriverWait(driver, 10)
+    page_reached = wait.until(ec.url_to_be(LOGIN_URL))
+
+    #USERNAME
+    username_input = driver.find_element(By.XPATH, "//input[@id='login_username']")
+    username_input.send_keys(username)
+
+    password_input = driver.find_element(By.XPATH, "//input[@id='login_password']")
+    password_input.send_keys(password)
+
+    login_button = driver.find_element(By.XPATH, "//button[@id='loginButton']")
+    login_button.click()
+
+    print("Sleeping for 20 seconds...")
+    time.sleep(20)
+    print("Sleep exited")
+
+
---
+++ scotty_gui.py
@@ -0,0 +1,69 @@
+"""GUI for test login process"""
+
+
+import sys
+import os
+import time
+import logging
+import logging.config
+
+from PyQt6.QtCore import Qt, QThread, QTimer, QObject, pyqtSignal
+from PyQt6.QtWidgets import (QApplication,
+                             QMainWindow,
+                             QLabel,
+                             QLineEdit,
+                             QGridLayout,
+                             QWidget,
+                             QPushButton)
+
+from login_simple import login_user
+
+class LoginGUI(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.setupUi()
+
+    def setupUi(self):
+        username_label = QLabel("Username:")
+        self.username_line_edit = QLineEdit()
+
+        password_label = QLabel("Password:")
+        self.password_line_edit = QLineEdit()
+        self.password_line_edit.setEchoMode(QLineEdit.EchoMode.Password)
+
+        run_button = QPushButton()
+        run_button.setText("Run Login Process")
+        run_button.pressed.connect(self.on_run_button_pressed)
+
+        central_widget = QWidget()
+
+        combined_layout = QGridLayout()
+
+        combined_layout.addWidget(username_label, 0 ,0)
+        combined_layout.addWidget(self.username_line_edit, 0, 1)
+        combined_layout.addWidget(password_label, 1, 0)
+        combined_layout.addWidget(self.password_line_edit, 1, 1)
+        combined_layout.addWidget(run_button, 2, 1)
+
+        central_widget.setLayout(combined_layout)
+
+        self.setCentralWidget(central_widget)
+
+    def on_run_button_pressed(self):
+        print("Running 'Login' process...")
+        un = self.username_line_edit.text()
+        pw = self.password_line_edit.text()
+
+        login_user(un, pw)
+        print("'Login' process complete!")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = LoginGUI()
+    window.show()
+
+    app.exec()
